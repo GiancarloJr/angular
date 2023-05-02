ea645b70446c97eb6596b941d4c50764e765ff88
GiancarloJr <108488305+GiancarloJr@users.noreply.github.com>
2023-05-02 11:50:58 -0300

    project minhas financas

---
+++ minhas-financas/minhas-financas/src/app/features/dashboard/service/dashboard.service.ts
@@ -0,0 +1,23 @@
+import { Injectable, Injector } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpBaseService } from 'src/app/shared/base/http-base.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DashboardService extends HttpBaseService{
+
+  private endpoint = 'entradas'
+
+  constructor(protected override readonly injector: Injector) {
+    super(injector);
+  }
+
+
+  getEntradas(payload?: any): Observable<any>{
+
+    const params = payload ? `?q=${payload.mes}/${payload.ano}` :'';
+
+    return this.httpGet(`${this.endpoint}${params}`);
+  }
+}
---
+++ minhas-financas/minhas-financas/src/app/features/entradas/pipes/status.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'status'
+})
+export class StatusPipe implements PipeTransform {
+
+  transform(value: boolean, ...args: unknown[]): string {
+
+    return value ? 'Pago': 'Pendente';
+  }
+
+}
